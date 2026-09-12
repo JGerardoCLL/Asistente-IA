f64cab9f9939b98050ae428cb6e53f6df6aae35c
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-12 15:46:25 -0600

    usar la conexion con bd

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const pool = require('./db');
+const pool = require('./src/db/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
